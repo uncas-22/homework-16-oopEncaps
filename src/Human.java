0274672c53c225913a6f24918f857f1678e1b2ba
Alex <seredin-alex@mail.ru>
2022-11-29 21:02:23 +0300

    наполнил класс Human

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,60 @@
 public class Human {
+    private String name;
+    private int yearOfBirth;
+    private String town;
+    private String jobTitle;
 
+    public Human(String name, int yearOfBirth, String town, String jobTitle) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.town = town;
+        this.jobTitle = jobTitle;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getYearOfBirth() {
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+        return this.yearOfBirth;
+    }
+
+    public void  setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+    }
+
+    public String getTown() {
+        return this.town;
+    }
+
+    public void setTown (String town) {
+        if (town != null && !town.isEmpty() && !town.isBlank()) {
+            this.town = town;
+        } else {
+            this.town = "информация не указана";
+        }
+    }
+
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobTitle() {
+        return this.jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + getName() + ". Я из города " + getTown() + ". Я родился " + getYearOfBirth() + " в году. Я работаю на должности: " + getJobTitle() + ". Будем знакомы!";
+    }
 }
